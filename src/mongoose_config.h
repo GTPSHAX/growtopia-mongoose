1796e1590c657586c59e05c871f8102ac1c14278
GTPSHAX <rafiehasannudin@gmail.com>
2026-06-06 14:51:45 +0700

    refactor(config): remove unused standard and platform-specific headers

--- src/mongoose_config.h
+++ src/mongoose_config.h
@@ -2,66 +2,6 @@
 #define MONGOOSE_CONFIG_H
 
 /*
- * Standard C headers required by Mongoose when using MG_ARCH_CUSTOM.
- */
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-/*
- * Platform-specific headers and compatibility macros.
- */
-#if defined(_WIN32) || defined(_WIN64)
-  #include <winsock2.h>
-  #include <ws2tcpip.h>
-  #include <windows.h>
-  #include <io.h>
-  #include <direct.h>
-
-  #if defined(_MSC_VER)
-    #include <malloc.h>
-    #ifndef alloca
-      #define alloca(size) _alloca(size)
-    #endif
-  #endif
-
-  #define mkdir(path, mode) _mkdir(path)
-
-  #define MG_CUSTOM_NONBLOCK(fd) do { \
-    unsigned long non_blocking = 1; \
-    ioctlsocket(fd, FIONBIO, &non_blocking); \
-  } while (0)
-#else
-  #ifndef _GNU_SOURCE
-    #define _GNU_SOURCE
-  #endif
-  #include <alloca.h>
-  #include <unistd.h>
-  #include <sys/types.h>
-  #include <sys/stat.h>
-  #include <sys/socket.h>
-  #include <sys/time.h>
-  #include <sys/select.h>
-  #include <sys/ioctl.h>
-  #include <netinet/in.h>
-  #include <netinet/tcp.h>
-  #include <arpa/inet.h>
-  #include <netdb.h>
-  #include <net/if.h>
-  #include <dirent.h>
-  #include <signal.h>
-#endif
-
-/*
  * Set to 0 for desktop OS environments to use the native socket stack.
  */
 #define MG_ENABLE_TCPIP 0
